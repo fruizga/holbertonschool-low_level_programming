5b1ab441c966095122f5f6382e7d1fd129d1ec7c
fruizga <ruizgallego@gmail.com>
2019-09-24 20:21:39 +0000

    fix alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,14 @@
 /**
  *print_alphabet -  use putchar
  *
- *
+ *Description: Print the alphabet
  */
 void print_alphabet(void)
 {
 int lt;
-for (lt = 'a'; lt <= 'z'; lt ++)
+for (lt = 'a'; lt <= 'z'; lt++)
 {
 _putchar(lt);
 }
 _putchar('\n');
 }
-
