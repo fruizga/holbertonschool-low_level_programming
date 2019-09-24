5d949744843404cf9bb289355d51c3643050b7fa
fruizga <ruizgallego@gmail.com>
2019-09-24 20:10:53 +0000

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet -  use putchar
+ *
+ *
+ */
+void print_alphabet(void)
+{
+int lt;
+for (lt = 'a'; lt <= 'z'; lt ++)
+{
+_putchar(lt);
+}
+_putchar('\n');
+}
+
