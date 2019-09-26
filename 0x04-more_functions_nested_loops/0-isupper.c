738338e61d5ec1b3a3bc0d1d80756d117f8c3159
fruizga <ruizgallego@gmail.com>
2019-09-26 15:38:41 +0000

    Isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks if the letter is upper
+ *@c:This is the character to check
+ * Description: finds if the case is lower
+ * Return: 1 if c is upper or 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c <= 'Z' && c >= 'A')
+return (1);
+else
+return (0);
+}
