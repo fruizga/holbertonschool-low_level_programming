d7967cff2821a05eb261a46f4778217260fb9f06
fruizga <ruizgallego@gmail.com>
2019-10-01 19:04:20 +0000

    First pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - Change de value of a pointer
+ *Description: Change value.
+ *@n: pointer as parameter
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
