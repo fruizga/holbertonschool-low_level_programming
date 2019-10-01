ee05ba3532442a8bbfb94bfda568f9cc33a7bb72
fruizga <ruizgallego@gmail.com>
2019-10-01 20:01:54 +0000

    Fixing some vainas

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *_strlen - Returns Array length
+ *Description: Returns length of an array
+ *@s: array
+ *@len: variable to iterate
+ */
+int _strlen(char *s)
+{
+int len;
+for (len = 0; s[len]; len++)
+{
+}
+return (len);
+}
