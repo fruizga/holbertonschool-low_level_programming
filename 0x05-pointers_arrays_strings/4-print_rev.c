5848972b000bb1d3497a07f6416bc086deca6133
fruizga <ruizgallego@gmail.com>
2019-10-01 22:32:03 +0000

    Print array in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string
+ *Description: prits all characters of a string
+ *@str: pointer as param
+ *
+ */
+void print_rev(char *str)
+{
+int i;
+for (i = 0; str[i]; i++)
+{
+
+}
+while (i-- > 0)
+{
+	_putchar(str[i]);
+}
+_putchar('\n');
+}
