a70de6f636e87145b1d95b264da7afc0e0f4d29f
fruizga <ruizgallego@gmail.com>
2019-10-03 17:04:32 +0000

    Fix strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,21 +4,24 @@
  * *_strcat - Concatenate two strings
  *@dest: Array that receive the other array
  *@src: Array to be added
+ *
  *Return: Always 0.
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i, len1 = 0;
-
-	for (i = 0; dest[i]; i++)
-	{
-		len1++;
+	int i, j,  len1 = 0;
+while (dest[i]  != '\0')
+{
+len1++;
+i++;
 }
 
-	for (i = 0; i < len1 && src[i] != '\0'; i++)
+for (j = 0; j < len1; j++)
 	{
-		dest[len1 + i] = src[i];
-		dest[len1 + i]  = '\0';
+		dest[len1 + j] = src[j];
 }
-	return dest;
+dest[len1 + j] = '\0';
+	return (dest);
+
 }
