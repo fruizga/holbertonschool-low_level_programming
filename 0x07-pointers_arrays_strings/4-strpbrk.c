3e7e503e65dce94876154762d33aab71c6159d6b
fruizga <ruizgallego@gmail.com>
2019-10-07 22:05:43 +0000

    ocurrency of any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ',')
+		{
+			break;
+		}
+		else
+		{
+			for (j = 0; accept[j]; j++)
+			{
+				if (s[i] == accept[j] && s[i] != ',')
+				{
+					s = s + i;
+				}
+			}
+		}
+	}
+	return (s);
+}
