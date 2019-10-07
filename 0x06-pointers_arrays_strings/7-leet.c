d17f7311666d804ba6e461bbbff659ab47b47d3e
fruizga <ruizgallego@gmail.com>
2019-10-06 22:53:51 -0500

    fixing

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ char *leet(char *str)
 	char a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char b[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	index = 0;
+	i= 0;
 	while (str[i] != '\0')
 	{
 		cnt = 0;
