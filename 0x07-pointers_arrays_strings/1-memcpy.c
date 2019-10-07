6ce2c6f5540ad2d278f30fe2b7b8d3899d13d8ae
fruizga <ruizgallego@gmail.com>
2019-10-07 17:10:09 +0000

    Copy n bytes from src to dest

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,19 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * *_memcpy - copy n bytes from src to dest
+ *@n: number of bytes
+ *@src: string to copy
+ *@dest: store _memcpy result
+ *Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i = 0;
 
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
 }
