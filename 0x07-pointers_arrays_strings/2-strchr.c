ad5cda4a2f6ad61d9d11fb313beceec069e1d26c
fruizga <ruizgallego@gmail.com>
2019-10-07 19:39:54 +0000

    change pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * _strchr - Locates a character in a string
+ *@s: string
+ *@c: character to find
+ *Return: matches
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			s = s + i;
+			return (s);
+}
+		i++;
+}
+	return (s);
+}
