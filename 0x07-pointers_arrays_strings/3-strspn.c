42a9b34d5c01c519f75cfcdd348a23223191deab
fruizga <ruizgallego@gmail.com>
2019-10-07 20:10:03 +0000

    length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strspn - counting prefix bytes
+ *@s: prefix
+ *@accept: characters to find
+ *Return: only success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, cnt = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ',')
+		{
+			break;
+}
+		else
+		{
+		for (j = 0; accept[j]; j++)
+		{
+if (s[i] == accept[j] && s[i] != ',')
+			{
+				cnt++;
+}
+}
+}
+	}
+	return (cnt);
+}
