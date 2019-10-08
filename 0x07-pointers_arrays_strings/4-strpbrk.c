74cee69e5fb9b1fc5c220b6f0f12c2ddebe37105
fruizga <ruizgallego@gmail.com>
2019-10-07 21:26:26 -0500

    Fix change of pointer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,30 +1,30 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * _strpbrk - changes the pointer
+ *@s: string to analyze
+ *@accept: pattern to find in s
+ * Return: new pointer
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+unsigned int i, j;
 
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] == ',')
 		{
 			break;
-		}
+}
 		else
-		{
-			for (j = 0; accept[j]; j++)
-			{
-				if (s[i] == accept[j] && s[i] != ',')
-				{
-					s = s + i;
-				}
-			}
-		}
-	}
-	return (s);
+{
+for (j = 0; accept[j]; j++)
+{
+if (s[i] == accept[j] && s[i] != ',')
+{
+	s = s + i;
+}
+}
+}
+}
+return (s);
 }
