8622e28c2c1c753a4105c235233b3449f9dc256e
fruizga <ruizgallego@gmail.com>
2019-10-09 19:46:10 +0000

    trying to raise a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_pow_recursion - x raised y
+ *@x: number
+ *@y: power
+ *Return: num pow and -1 if y = 0
+ */
+int _pow_recursion(int x, int y)
+{
+	int r = 0;
+
+	if (y == 0)
+	{
+		return (-1);
+}
+	else if (y > 0)
+	{
+		return (r = x * _pow_recursion(x, (y - 1)));
+}
+	else
+	{
+		return (1);
+}
+}
