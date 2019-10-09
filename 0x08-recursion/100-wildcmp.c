05da6d87a35edef0b207b9a9c49df53b3ccc7ea1
fruizga <ruizgallego@gmail.com>
2019-10-09 19:16:57 +0000

    String compare and define ifthy

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *wildcmp - compare two strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: 0 if the strings are not identical
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+}
