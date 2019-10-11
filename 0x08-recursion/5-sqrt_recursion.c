47bc3042f85d241898b59686b20ff6155d9bc4de
fruizga <ruizgallego@gmail.com>
2019-10-10 23:08:21 -0500

    Calculate sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,17 +5,18 @@
  *Return: something
  *
  */
-int _sqrt_recursion(int n);
-{
-int i = 0;
-return (_parte_dos(n, i));
-}
-
 int _parte_dos(int n, int i)
 {
 if (i * i > n)
 	return (-1);
 if (i * i == n)
 	return (i);
-	return (_sqrt_recursion2(n, i + 1));
+	return (_parte_dos(n, i + 1));
+}
+
+
+int _sqrt_recursion(int n);
+{
+int i = 0;
+return (_parte_dos(n, i));
 }
