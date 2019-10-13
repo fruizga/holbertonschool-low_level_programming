1cffe74c4036b15d2bfe21cabe5495714970863b
fruizga <ruizgallego@gmail.com>
2019-10-13 12:49:09 -0500

    Betty was asking for more

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,14 @@ int main(int argc, char *argv[])
 int multi;
 
 if (argc != 3)
-   {
-     printf("Error\n");
-     return (1);
-   }
- else
-   {
-     multi = atoi(argv[1]) * atoi(argv[2]);
-     printf("%d\n", multi);
-     return (0);
-   }
-
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+multi = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", multi);
+return (0);
+}
 }
