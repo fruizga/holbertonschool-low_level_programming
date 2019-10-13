b84c35a65ae3e52961d30f7d3306db3c38617587
fruizga <ruizgallego@gmail.com>
2019-10-13 14:42:13 -0500

    fix Sum positives

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,21 +4,21 @@
 * main - Multiply two arguments
 *@argv: array of arguments
 *@argc: arg count
+*Return: 0
 */
 int main(int argc, char *argv[])
 {
 int multi;
 
 if (argc != 3)
-   {
-     printf("Error\n");
-     return (1);
-   }
- else
-   {
-     multi = atoi(argv[1]) * atoi(argv[2]);
-     printf("%d\n", multi);
-     return (0);
-   }
-
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+multi = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", multi);
+return (0);
+}
 }
