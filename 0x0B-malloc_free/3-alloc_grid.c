122a34bd6236e02790f3217b18741d9c028ea788
fruizga <ruizgallego@gmail.com>
2019-10-16 18:40:10 +0000

    Alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,41 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- *
- *
- *
- *
+ *alloc_grid - return an array pointer
+ *@width: El ancho
+ *@height: El alto
+ *Return: pointer if is right or NULL if something is wrong
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **pt;
+pt = malloc(height * sizeof(int *));
 
+if (pt == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i <= height; i++)
+{
+	pt[i] = malloc(width * sizeof(int));
+
+	if (pt[i] == NULL)
+	{
+		for (j = i; j >= 0; j++)
+		{
+			free(pt[j]);
+}
+		free(pt);
+}
+}
+for (i = 0; i < height; i++)
+{
+	for (j = 0; j < width; j++)
+	{
+		pt[i][j] = 0;
+}
+}
+return (pt);
 }
