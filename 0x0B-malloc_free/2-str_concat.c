281c1832dfd3fda1053b172e4ba0d2c38791125f
fruizga <ruizgallego@gmail.com>
2019-10-16 14:07:14 +0000

    no sirvió

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,16 +12,6 @@ char *str_concat(char *s1, char *s2)
 	int i, j, h, k = 0,  len1 = 0, sum = 0;
 	char *pt;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-}
-
-	if (s2 == NULL)
-	{
-		s2 = "";
-}
-
 	while (s1[i]  != '\0')
 	{
 		i++;
