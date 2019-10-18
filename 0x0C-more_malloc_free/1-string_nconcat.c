d7aca0d695e107b11c2eb8ab47264e86f24c8754
fruizga <ruizgallego@gmail.com>
2019-10-17 23:00:30 -0500

    Fix concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,53 +2,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *string_nconcat - concatenate two strings
- *@s1: string one
- *@s2: string two, to be added
- *@n: number of bytes to be concatenated
- *Return: pointer when everything is ok
- */
+*string_nconcat - concatenates two string
+*@s1: string 1
+*@s2: string 2
+*@n: number of bytes to concatenate
+*Return: pointer to char
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, suma = 0, b = 0;
-	char *ptr;
-
-	if (s1 == NULL)
-	{
-		s1 = "";
-}
-	if (s2 == NUL)
-	{
-		s2 = "";
-}
-	while (s1[i] == 0)
-	{
-		i++;
-}
-	while (s2[j])
-	{
-		j++;
-}
-	if (n >= j)
-		n = j;
-	ptr = malloc(sizeof(char) * (i + j + 1));
-		suma = i + j;
-	else
-		ptr = malloc(sizeof(char) * (i + n + 1));
-	suma = i + n;
-}
-	if (ptr == NULL)
-	{
-		return (NULL);
-}
-for (k = 0 ; k < suma; k++)
+char *ptr;
+unsigned int i = 0, j = 0, a = 0, b = 0;
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+while (s1[i])
+	i++;
+while (s2[j])
+	j++;
+if (n >= j)
 {
-	if (k < i)
-		ptr[k] = s1[k];
-	else
-		ptr[k] = s2[b];
-	b++;
+	n = j;
+ptr = malloc(sizeof(char) * (i + j + 1));
 }
-ptr[i + k] = '\0';
+else
+{
+	ptr = malloc(sizeof(char) * (n + i + 1));
+}
+if (ptr == NULL)
+	return (NULL);
+for (a = 0; a < i; a++)
+{
+	ptr[a] = s1[a];
+}
+for (b = 0; b < n; b++)
+{
+	ptr[a++] = s2[b];
+}
+ptr[a++] = '\0';
 return (ptr);
 }
