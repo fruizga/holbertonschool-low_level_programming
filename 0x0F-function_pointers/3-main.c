7fd4a9904ca0b08d40df1cbbc4359cb419e7dc63
fruizga <ruizgallego@gmail.com>
2019-10-23 18:46:20 -0500

    Ya casi

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,5 +8,30 @@
  */
 int main(int argc, char *argv[])
 {
-	
+	int x = 0, y = 0, z = 0;
+
+	int (*p)(int, int);
+
+	if (argc !=  4)
+	{
+		printf("Error\n" );
+		exit(98);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	p = get_op_func(argv[2]);
+
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	z = p(x, y);
+
+	print("%i\n", z);
+
+	return (0);
+
 }
