93c9d104ef171628c2bdebc80ef86e501f2acd60
fruizga <ruizgallego@gmail.com>
2019-10-23 15:57:49 -0500

    Fix absolute output

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,12 +14,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (cmp != NULL && array != NULL)
+	{
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 		{
 			return (i);
 		}
+	}
 	}
 	return (-1);
 }
