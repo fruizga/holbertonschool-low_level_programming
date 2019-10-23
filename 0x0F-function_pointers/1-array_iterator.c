442653b8db28789a290bc2fb41dec2e88dd59f53
fruizga <ruizgallego@gmail.com>
2019-10-23 13:41:51 -0500

    Review output

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,10 +9,13 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
-	
+
+	if (array != NULL && size > 0 && action !=NULL)
+	{
 	while (i < size)
 	{
 		action(array[i]);
 			i++;
+	}
 	}
 }
