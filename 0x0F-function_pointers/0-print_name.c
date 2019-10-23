126e1259269a496a3b4f1759763c31e9356921cd
fruizga <ruizgallego@gmail.com>
2019-10-23 10:36:23 -0500

    Starting the folder

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *print_name -prints the name
+ *@name:name as pointer
+ *@f: pointer
+ *Return: 0 if everything is ok
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
