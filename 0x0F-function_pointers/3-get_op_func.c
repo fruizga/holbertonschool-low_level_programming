f1349941aa0e12f9ef2543d33b73e898255d92d6
fruizga <ruizgallego@gmail.com>
2019-10-24 00:01:03 -0500

    comentando funciones

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- *
- *
- *
- */
+*get_op_func - pointer to get the character of operation
+*@s: string of operators
+*Return: NULL if operartion doesn't exist'
+*/
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
 
-    while (ops[i].op)
-    {
-	    if (ops[i].op == s)
-	    {
-		    return ((ops[i].f);
-	    }
-			    i++;
-    }
-	    return (NULL);
-  }
+while (ops[i].op)
+{
+	if (ops[i].op == s)
+	{
+		return (ops[i].f);
+	}
+			i++;
+}
+	return (NULL);
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,31 +1,63 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- *
- *
- *
- *
- *
- *
- *
- */
+ *op_add - sum of two integers
+ *@a: first number
+ *@b: second number
+ *Return: Result
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ *op_sub - subtraction of two integers
+ *@a: first number
+ *@b: second number
+ *Return: Result
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ *op_mul - multiplies two integers
+ *@a: first number
+ *@b: second number
+ *Return: Result
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ *op_div - division of two integers
+ *@a: first number
+ *@b: second number
+ *Return: Result
+*/
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+
+}
 	return (a / b);
 }
+/**
+ *op_mod - modulo of two integers
+ *@a: first number
+ *@b: second number
+ *Return: Result
+*/
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
