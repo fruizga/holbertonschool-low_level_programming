8395d37a37e97042111804cd789509d341afaa8d
fruizga <ruizgallego@gmail.com>
2019-10-27 20:09:16 -0500

    Fix item 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,40 +8,36 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	int x = 0;
-	char *MyArr;
+va_list valist;
+	int i = 0;
+	char *s;
 
 	va_start(valist, format);
-
-	while (format[x] != '\0')
+	while (format && format[i])
 	{
-		switch (format[x])
+		switch (format[i++])
 		{
-		case 'c':
-			printf("%c", va_arg(valist, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double));
-			break;
-		case 's':
-			MyArr = va_arg(valist, char*);
-			if (MyArr != NULL)
-			{
-				printf("%s", MyArr);
+			case 'c':
+				printf("%c", va_arg(valist, int));
 				break;
-			}
-			{printf("(nil)"); }
-			break;
+			case 'i':
+				printf("%i", va_arg(valist, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double));
+				break;
+			case 's':
+				s = va_arg(valist, char*);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+			default:
+				continue;
 		}
-		if (format[(x + 1)] != '\0' && (format[x] == 'c' ||
-		format[x] == 'i' || format[x] == 'f' || format[x] == 's'))
-		{printf(", "); }
-		x++;
+		if (format[i])
+			printf(", ");
 	}
-	printf("\n");
+	putchar('\n');
 	va_end(valist);
 }
