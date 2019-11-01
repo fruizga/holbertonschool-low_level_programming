ab7fe82dcff09005e75c940fc9d20adf4ed9e025
fruizga <ruizgallego@gmail.com>
2019-11-01 00:21:36 -0500

    fixing code identation

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,24 @@
 #include "lists.h"
 
 /**
- * print_list - prints elements of list
- * @h: pointer to list_t
- * Description: prints elements of a list
- * Return: number of nodes.
- */
+* print_list - prints elements of list
+* @h: pointer to list_t
+* Description: prints elements of a list
+* Return: number of nodes.
+*/
 size_t print_list(const list_t *h)
 {
-	int nodo;
+int nodo;
 
-       	for (nodo = 0; h; nodo++)
-	{
-		if ((*h).str == NULL)
-		{printf("[0] (nil)\n"); }
-		else
-		{printf("[%d] %s\n", (*h).len, (*h).str); }
-		h = (*h).next;
-	}
-	return (nodo);
+for (nodo = 0; h; nodo++)
+{
+if ((*h).str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{printf("[%d] %s\n", (*h).len, (*h).str); }
+h = (*h).next;
+}
+return (nodo);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,10 @@
 #ifndef LIST_S
 #define LIST_S
 
+#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdarg.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -22,5 +22,10 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 
 #endif
