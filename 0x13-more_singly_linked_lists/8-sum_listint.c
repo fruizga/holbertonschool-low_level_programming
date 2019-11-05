e0ada07aab6ac8cdfd61c6a2e922b2af1f30da9e
fruizga <ruizgallego@gmail.com>
2019-11-05 09:41:05 -0500

    add sum of nodes

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *sum_listint - sum all data of the list
+ *@head: pointer to head of list
+ *Return: 0 if list is empty else if there are nodes
+ */
+int sum_listint(listint_t *head)
+{
+	int cnt = 0;
+	listint_t *h;
+	h = head;
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+	while (h)
+	{
+		cnt  = cnt + h->n;
+		h = h->next;
+
+	}
+
+
+	return (cnt);
+	}
+}
