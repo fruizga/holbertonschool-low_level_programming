5117d8e719d15b694d45bc6cfbc927f629f92ac5
fruizga <ruizgallego@gmail.com>
2019-11-05 14:55:32 -0500

    partial node delete

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ *delete_nodeint_at_index - delete a singular node
+ *@head: pointer to head of list
+ *@index: position of node to delete
+ *Return: Success or no
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *h, *del;
+	unsigned int pos = 0;
+
+	h = *head;
+	if (h == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		del = h;
+		h = h->next;
+		free(del);
+	}
+	while (pos < (index - 1))
+	{
+		h = h->next;
+		pos++;
+	}
+	del = h->next;
+	h->next = del->next;
+	free(del);
+	return (1);
+}
