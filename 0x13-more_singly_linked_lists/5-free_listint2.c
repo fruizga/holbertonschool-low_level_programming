e4aa5765a77112ada4a15bae1700ad7a8114e951
fruizga <ruizgallego@gmail.com>
2019-11-05 09:00:50 -0500

    Betty doesn't like my code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
- *
- *
+ * free_listint2 - free list
+ *@head: pointer to head of list
  *
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	while (*head != NULL )
+	while (*head != NULL)
 	{
 
 		aux = *head;
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,12 +3,12 @@
  *get_nodeint_at_index - returns the nth node of a list
  *@head:pointer to head list
  *@index:node position
-Return: ntn node of the list
+ *Return: ntn node of the list
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *h= head;
+	listint_t *h = head;
 
 	if (h != NULL)
 	{
