8357dcaf9cb67aa2a48c5bc832e7f99288669307
fruizga <ruizgallego@gmail.com>
2019-11-13 15:08:14 -0500

    fix error output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,13 +16,13 @@ main(int argc, char *argv[])
 
     if (argc != 3 )
     {
-        dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit (97);
     }
     inputFd = open(argv[1], O_RDONLY);
     if (inputFd == -1)
     {
-        dprintf(STDOUT_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+        dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
         exit (98);
     }
     openFlags = O_CREAT | O_WRONLY | O_TRUNC;
@@ -31,29 +31,29 @@ main(int argc, char *argv[])
     outputFd = open(argv[2], openFlags, filePerms);
     if (outputFd == -1)
     {
-        dprintf(STDOUT_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+        dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
         exit (99);
     }
     while ((numRead = read(inputFd, buff, BUF_SIZE)) > 0)
     if (write(outputFd, buff, numRead) != numRead)
     {
-        dprintf(STDOUT_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+        dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
         exit (99);
     }
     if (numRead == -1)
     
     {
-       dprintf(STDOUT_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+       dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
         exit (99); 
     }
     if (close(inputFd) == -1)
     {
-        dprintf(STDOUT_FILENO, "Error: Can't close fd FD_VALUE\n");
+        dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
         exit (100); 
     }
     if (close(outputFd) == -1)
     {
-        dprintf(STDOUT_FILENO, "Error: Can't close fd FD_VALUE\n");
+        dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
         exit (100); 
     } 
 }
