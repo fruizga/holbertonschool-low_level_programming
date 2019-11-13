4b2e98fea7d089ed5fc6634ef3f8f904c31c99c9
fruizga <ruizgallego@gmail.com>
2019-11-13 15:19:22 -0500

    fix betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, cnt = 0, writer = 0;
-	
+
 	if (filename == NULL)
 	{
 		return (-1);
@@ -25,7 +25,6 @@ int create_file(const char *filename, char *text_content)
 		if (writer == -1)
 			return (-1);
 	}
-		
 	close(fd);
 	return (1);
 }
